80114549e3f6a77651a15d63e4bec6f88586990b
Roberto-Ceschini <robert17ceschini@gmail.com>
2024-12-26 18:01:04 -0300

    Update create-usuario.dto.ts
    Swagger + DTO do usuario

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,7 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUsuarioDto {
-    
+
+    @ApiProperty({
+        description: "nome do usuário",
+        example: "Joao do pe de feijao"
+    })
       nome: string;
+
+      @ApiProperty({
+        description: "email do usuário",
+        example: "email@email.com"
+    })
       email: string;
+
+      @ApiProperty({
+        description: "telefone do usuario",
+        example: "(21)2345678"
+    })
       telefone: string;
+
+      @ApiProperty({
+        description: "lista de datas disponiveis para ir nas palestras",
+        example: "12/12/2025 21/07/2001"
+    })
       datas_disponiveis: Date;
 }
