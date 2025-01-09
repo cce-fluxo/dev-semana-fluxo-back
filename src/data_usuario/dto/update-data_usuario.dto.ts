f8574ef7c7739c949ac324d5dcf6d1ca618d9e13
Roberto-Ceschini <robert17ceschini@gmail.com>
2025-01-09 12:31:00 -0300

    Exclusão entidade Data_usuario

--- src/data_usuario/dto/update-data_usuario.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDataUsuarioDto } from './create-data_usuario.dto';
-
-export class UpdateDataUsuarioDto extends PartialType(CreateDataUsuarioDto) {}
